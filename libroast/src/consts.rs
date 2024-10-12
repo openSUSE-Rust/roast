1b16d0963205aa02460b7c19b940b935e145faae
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2024-10-12 12:34:52 +0800

    consts: remove unnecessary consts

--- libroast/src/consts.rs
+++ libroast/src/consts.rs
@@ -1,6 +1,3 @@
-pub const XZ_EXTS: &[&str] = &["xz"];
-pub const ZST_EXTS: &[&str] = &["zstd", "zst"];
-pub const GZ_EXTS: &[&str] = &["gz", "gzip"];
 pub const XZ_MIME: &str = "application/x-xz";
 pub const ZST_MIME: &str = "application/zstd";
 pub const GZ_MIME: &str = "application/gzip";

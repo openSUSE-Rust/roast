487877a14b3169eb5f8a38c503734b9ac937a259
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2024-10-12 11:58:43 +0800

    minor: add Display trait to namespace and slightly change the error message

--- libroast/src/common.rs
+++ libroast/src/common.rs
@@ -1,5 +1,11 @@
 use clap::ValueEnum;
-use std::path::PathBuf;
+use std::{
+    fmt::{
+        self,
+        Display,
+    },
+    path::PathBuf,
+};
 
 #[derive(ValueEnum, Default, Debug, Clone, Copy)]
 pub enum Compression
@@ -16,4 +22,17 @@ pub enum SupportedFormat
 {
     Compressed(Compression, PathBuf),
     Dir(PathBuf),
+}
+
+pub struct UnsupportedFormat
+{
+    pub ext: String,
+}
+
+impl Display for UnsupportedFormat
+{
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result
+    {
+        write!(f, "Unsupported archive format: {}", self.ext)
+    }
 }

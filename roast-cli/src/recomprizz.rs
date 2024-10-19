ec8cfcdf2a29c1fa9d2ba82076d0987ec90a5c2b
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2024-10-19 21:16:58 +0800

    fix: apply clippy lints
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

--- roast-cli/src/recomprizz.rs
+++ roast-cli/src/recomprizz.rs
@@ -1,25 +1,43 @@
 use crate::{
-    cli::{RawArgs, RecomprizzArgs, RoastArgs},
+    cli::{
+        RawArgs,
+        RecomprizzArgs,
+        RoastArgs,
+    },
     raw::raw_opts,
     roast::roast_opts,
 };
 use clap::Parser;
 use std::{
     io,
-    path::{Path, PathBuf},
+    path::{
+        Path,
+        PathBuf,
+    },
 };
-use terminfo::capability as cap;
-use terminfo::Database;
+use terminfo::{
+    capability as cap,
+    Database,
+};
 #[allow(unused_imports)]
-use tracing::{debug, error, info, trace, warn, Level};
+use tracing::{
+    debug,
+    error,
+    info,
+    trace,
+    warn,
+    Level,
+};
 use tracing_subscriber::EnvFilter;
 
-pub fn recomprizz_cli_stub() -> io::Result<()> {
+pub fn recomprizz_cli_stub() -> io::Result<()>
+{
     let recomprizz_args = RecomprizzArgs::parse();
     recomprizz_opts(recomprizz_args)
 }
 
-pub(crate) fn recomprizz_opts(recomprizz_args: RecomprizzArgs) -> io::Result<()> {
+pub(crate) fn recomprizz_opts(recomprizz_args: RecomprizzArgs) -> io::Result<()>
+{
     let terminfodb = Database::from_env().map_err(|e| {
         error!(err = ?e, "Unable to access terminfo db. This is a bug!");
         io::Error::new(
@@ -28,7 +46,8 @@ pub(crate) fn recomprizz_opts(recomprizz_args: RecomprizzArgs) -> io::Result<()>
         )
     });
 
-    let is_termcolorsupported = match terminfodb {
+    let is_termcolorsupported = match terminfodb
+    {
         Ok(hasterminfodb) => hasterminfodb.get::<cap::MaxColors>().is_some(),
         Err(_) => false,
     };
@@ -40,9 +59,12 @@ pub(crate) fn recomprizz_opts(recomprizz_args: RecomprizzArgs) -> io::Result<()>
         .with_env_filter(filter_layer)
         .with_level(true);
 
-    let builder = if cfg!(debug_assertions) {
+    let builder = if cfg!(debug_assertions)
+    {
         builder.with_file(true).with_line_number(true)
-    } else {
+    }
+    else
+    {
         builder
     };
 
@@ -56,29 +78,29 @@ pub(crate) fn recomprizz_opts(recomprizz_args: RecomprizzArgs) -> io::Result<()>
     })?;
     let outpath_for_raw = &tmp_binding_for_raw.path();
 
-    let tmp_binding_for_roast = tempfile::TempDir::new().map_err(|err| {
-        error!(?err, "Failed to create temporary directory");
-        err
-    })?;
-    let outpath_for_roast = &tmp_binding_for_roast.path();
-
-    let raw_args =
-        RawArgs { target: recomprizz_args.target, outpath: Some(outpath_for_raw.to_path_buf()) };
+    let raw_args = RawArgs {
+        target: recomprizz_args.target.clone(),
+        outpath: Some(outpath_for_raw.to_path_buf()),
+    };
 
     raw_opts(raw_args)?;
 
-    let mut out_filename = match recomprizz_args.rename {
-        Some(v) => PathBuf::from(v),
-        None => {
+    // Yuck!
+    let out_filename = match recomprizz_args.rename
+    {
+        Some(ref v) => &Path::new(v).to_path_buf(),
+        None => &{
             let mut filename = recomprizz_args.target.clone();
-            while let Some(file_prefix) = filename.file_stem() {
-                filename = PathBuf::from(file_prefix);
+            while let Some(file_prefix) = &mut filename.file_stem()
+            {
+                filename = PathBuf::from(&file_prefix);
             }
             filename
-        }
+        },
     };
 
-    let file_extension = match recomprizz_args.compression {
+    let file_extension = match recomprizz_args.compression
+    {
         libroast::common::Compression::Gz => "tar.gz",
         libroast::common::Compression::Xz => "tar.xz",
         libroast::common::Compression::Zst => "tar.zst",
@@ -86,12 +108,10 @@ pub(crate) fn recomprizz_opts(recomprizz_args: RecomprizzArgs) -> io::Result<()>
         libroast::common::Compression::Not => "tar",
     };
 
-    out_filename = out_filename.with_extension(file_extension);
-
     let roast_args = RoastArgs {
         target: outpath_for_raw.to_path_buf(),
         additional_paths: None,
-        outpath: outpath_for_roast.to_path_buf(),
+        outpath: out_filename.with_extension(file_extension),
         preserve_root: false,
         reproducible: recomprizz_args.reproducible,
     };

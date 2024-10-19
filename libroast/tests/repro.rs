729114a5fc0ab1613032f6b2c8ce4ab2e4943ba0
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2024-10-19 19:13:39 +0800

    tests: use copy_dir_all as part of lib now instead
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

--- libroast/tests/repro.rs
+++ libroast/tests/repro.rs
@@ -3,7 +3,6 @@ use sha3::{
     Keccak256,
 };
 use std::{
-    fs,
     fs::read,
     io,
     path::{
@@ -23,46 +22,6 @@ use tracing::{
 use tracing_test::traced_test;
 use walkdir::WalkDir;
 
-fn copy_dir_all(src: impl AsRef<Path>, dst: &Path) -> Result<(), io::Error>
-{
-    debug!("Copying sources");
-    debug!(?dst);
-    fs::create_dir_all(dst)?;
-    for entry in fs::read_dir(src)?
-    {
-        let entry = entry?;
-        let ty = entry.file_type()?;
-        trace!(?entry);
-        trace!(?ty);
-        if ty.is_dir()
-        {
-            trace!(?ty, "Is directory?");
-            copy_dir_all(entry.path(), &dst.join(entry.file_name()))?;
-
-        // Should we respect symlinks?
-        // } else if ty.is_symlink() {
-        //     debug!("Is symlink");
-        //     let path = fs::read_link(&entry.path())?;
-        //     let path = fs::canonicalize(&path).unwrap();
-        //     debug!(?path);
-        //     let pathfilename = path.file_name().unwrap_or(OsStr::new("."));
-        //     if path.is_dir() {
-        //         copy_dir_all(&path, &dst.join(pathfilename))?;
-        //     } else {
-        //         fs::copy(&path, &mut dst.join(pathfilename))?;
-        //     }
-
-        // Be pedantic or you get symlink error
-        }
-        else if ty.is_file()
-        {
-            trace!(?ty, "Is file?");
-            fs::copy(entry.path(), dst.join(entry.file_name()))?;
-        };
-    }
-    Ok(())
-}
-
 const MANIFEST_DIR: &str = std::env!("CARGO_MANIFEST_DIR", "No such manifest dir");
 
 fn generate_gz_tarball(outpath: &Path) -> io::Result<()>
@@ -73,7 +32,7 @@ fn generate_gz_tarball(outpath: &Path) -> io::Result<()>
         err
     })?;
     let workdir = &tmp_binding.path();
-    copy_dir_all(src, workdir)?;
+    libroast::copy_dir_all(src, workdir)?;
     let updated_paths: Vec<PathBuf> = WalkDir::new(workdir)
         .into_iter()
         .filter_map(|entry| entry.ok())
@@ -98,7 +57,7 @@ fn generate_xz_tarball(outpath: &Path) -> io::Result<()>
         err
     })?;
     let workdir = &tmp_binding.path();
-    copy_dir_all(src, workdir)?;
+    libroast::copy_dir_all(src, workdir)?;
     let updated_paths: Vec<PathBuf> = WalkDir::new(workdir)
         .into_iter()
         .filter_map(|entry| entry.ok())
@@ -123,7 +82,7 @@ fn generate_zst_tarball(outpath: &Path) -> io::Result<()>
         err
     })?;
     let workdir = &tmp_binding.path();
-    copy_dir_all(src, workdir)?;
+    libroast::copy_dir_all(src, workdir)?;
     let updated_paths: Vec<PathBuf> = WalkDir::new(workdir)
         .into_iter()
         .filter_map(|entry| entry.ok())
@@ -148,7 +107,7 @@ fn generate_bz2_tarball(outpath: &Path) -> io::Result<()>
         err
     })?;
     let workdir = &tmp_binding.path();
-    copy_dir_all(src, workdir)?;
+    libroast::copy_dir_all(src, workdir)?;
     let updated_paths: Vec<PathBuf> = WalkDir::new(workdir)
         .into_iter()
         .filter_map(|entry| entry.ok())
@@ -173,7 +132,7 @@ fn generate_icecream_tarball(outpath: &Path) -> io::Result<()>
         err
     })?;
     let workdir = &tmp_binding.path();
-    copy_dir_all(src, workdir)?;
+    libroast::copy_dir_all(src, workdir)?;
     let updated_paths: Vec<PathBuf> = WalkDir::new(workdir)
         .into_iter()
         .filter_map(|entry| entry.ok())
--- libroast/tests/shame.rs
+++ libroast/tests/shame.rs
@@ -1,5 +1,4 @@
 use std::{
-    fs,
     io,
     path::{
         Path,
@@ -18,46 +17,6 @@ use tracing::{
 use tracing_test::traced_test;
 use walkdir::WalkDir;
 
-fn copy_dir_all(src: impl AsRef<Path>, dst: &Path) -> Result<(), io::Error>
-{
-    debug!("Copying sources");
-    debug!(?dst);
-    fs::create_dir_all(dst)?;
-    for entry in fs::read_dir(src)?
-    {
-        let entry = entry?;
-        let ty = entry.file_type()?;
-        trace!(?entry);
-        trace!(?ty);
-        if ty.is_dir()
-        {
-            trace!(?ty, "Is directory?");
-            copy_dir_all(entry.path(), &dst.join(entry.file_name()))?;
-
-        // Should we respect symlinks?
-        // } else if ty.is_symlink() {
-        //     debug!("Is symlink");
-        //     let path = fs::read_link(&entry.path())?;
-        //     let path = fs::canonicalize(&path).unwrap();
-        //     debug!(?path);
-        //     let pathfilename = path.file_name().unwrap_or(OsStr::new("."));
-        //     if path.is_dir() {
-        //         copy_dir_all(&path, &dst.join(pathfilename))?;
-        //     } else {
-        //         fs::copy(&path, &mut dst.join(pathfilename))?;
-        //     }
-
-        // Be pedantic or you get symlink error
-        }
-        else if ty.is_file()
-        {
-            trace!(?ty, "Is file?");
-            fs::copy(entry.path(), dst.join(entry.file_name()))?;
-        };
-    }
-    Ok(())
-}
-
 const MANIFEST_DIR: &str = std::env!("CARGO_MANIFEST_DIR", "No such manifest dir");
 
 #[test]
@@ -69,7 +28,7 @@ fn is_gz_tarball() -> io::Result<()>
         err
     })?;
     let workdir = &tmp_binding.path();
-    copy_dir_all(src, workdir)?;
+    libroast::copy_dir_all(src, workdir)?;
     let updated_paths: Vec<PathBuf> = WalkDir::new(workdir)
         .into_iter()
         .filter_map(|entry| entry.ok())
@@ -97,7 +56,7 @@ fn is_xz_tarball() -> io::Result<()>
         err
     })?;
     let workdir = &tmp_binding.path();
-    copy_dir_all(src, workdir)?;
+    libroast::copy_dir_all(src, workdir)?;
     let updated_paths: Vec<PathBuf> = WalkDir::new(workdir)
         .into_iter()
         .filter_map(|entry| entry.ok())
@@ -125,7 +84,7 @@ fn is_zst_tarball() -> io::Result<()>
         err
     })?;
     let workdir = &tmp_binding.path();
-    copy_dir_all(src, workdir)?;
+    libroast::copy_dir_all(src, workdir)?;
     let updated_paths: Vec<PathBuf> = WalkDir::new(workdir)
         .into_iter()
         .filter_map(|entry| entry.ok())
@@ -153,7 +112,7 @@ fn is_bz2_tarball() -> io::Result<()>
         err
     })?;
     let workdir = &tmp_binding.path();
-    copy_dir_all(src, workdir)?;
+    libroast::copy_dir_all(src, workdir)?;
     let updated_paths: Vec<PathBuf> = WalkDir::new(workdir)
         .into_iter()
         .filter_map(|entry| entry.ok())
@@ -181,7 +140,7 @@ fn is_vanilla_tarball() -> io::Result<()>
         err
     })?;
     let workdir = &tmp_binding.path();
-    copy_dir_all(src, workdir)?;
+    libroast::copy_dir_all(src, workdir)?;
     let updated_paths: Vec<PathBuf> = WalkDir::new(workdir)
         .into_iter()
         .filter_map(|entry| entry.ok())

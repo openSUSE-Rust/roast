31aefafa17ba6c01f6f25beab1191cd82ec5a6cc
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2024-10-19 18:22:58 +0800

    lib: move over copy_dir_all as a common utility
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

--- libroast/src/lib.rs
+++ libroast/src/lib.rs
@@ -6,6 +6,11 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+use std::{
+    fs,
+    io,
+};
+
 pub mod common;
 pub mod compress;
 pub mod consts;
@@ -32,8 +37,49 @@ use tracing::{
     debug,
     error,
     info,
+    trace,
     warn,
 };
+
+pub fn copy_dir_all(src: impl AsRef<Path>, dst: &Path) -> Result<(), io::Error>
+{
+    debug!("Copying sources");
+    debug!(?dst);
+    fs::create_dir_all(dst)?;
+    for entry in fs::read_dir(src)?
+    {
+        let entry = entry?;
+        let ty = entry.file_type()?;
+        trace!(?entry);
+        trace!(?ty);
+        if ty.is_dir()
+        {
+            trace!(?ty, "Is directory?");
+            copy_dir_all(entry.path(), &dst.join(entry.file_name()))?;
+
+        // Should we respect symlinks?
+        // } else if ty.is_symlink() {
+        //     debug!("Is symlink");
+        //     let path = fs::read_link(&entry.path())?;
+        //     let path = fs::canonicalize(&path).unwrap();
+        //     debug!(?path);
+        //     let pathfilename = path.file_name().unwrap_or(OsStr::new("."));
+        //     if path.is_dir() {
+        //         copy_dir_all(&path, &dst.join(pathfilename))?;
+        //     } else {
+        //         fs::copy(&path, &mut dst.join(pathfilename))?;
+        //     }
+
+        // Be pedantic or you get symlink error
+        }
+        else if ty.is_file()
+        {
+            trace!(?ty, "Is file?");
+            fs::copy(entry.path(), dst.join(entry.file_name()))?;
+        };
+    }
+    Ok(())
+}
 
 pub fn is_supported_format(src: &Path) -> Result<SupportedFormat, UnsupportedFormat>
 {

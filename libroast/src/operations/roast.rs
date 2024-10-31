60708fb088e244f88ce901eb4bb4bbb3fe5a599c
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2024-11-01 01:00:18 +0800

    fix: reimplement adding of archive files
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

--- libroast/src/operations/roast.rs
+++ libroast/src/operations/roast.rs
@@ -15,6 +15,7 @@ use std::{
         PathBuf,
     },
 };
+use tar::Entry;
 #[allow(unused_imports)]
 use tracing::{
     debug,
@@ -29,12 +30,11 @@ use walkdir::{
     WalkDir,
 };
 
-fn is_hidden(entry: &fs::DirEntry, hidden: bool, ignore_git: bool, root: &Path) -> bool
+fn is_hidden(entry: &Path, hidden: bool, ignore_git: bool, root: &Path) -> bool
 {
-    let binding = entry.file_name();
-    let entry_str = &binding.to_string_lossy();
+    let entry_str = entry.file_name().unwrap_or(entry.as_os_str());
+    let entry_str = entry_str.to_string_lossy();
     debug!(?entry_str, ?root);
-    let entry = &entry.path();
     let entry_canonicalized = entry.canonicalize().unwrap_or(entry.to_path_buf());
     let root_canonicalized = root.canonicalize().unwrap_or(root.to_path_buf());
     if entry_canonicalized == root_canonicalized
@@ -55,6 +55,64 @@ fn is_hidden(entry: &fs::DirEntry, hidden: bool, ignore_git: bool, root: &Path) 
     }
 }
 
+fn helper_archiver(
+    entry_as_path_canonicalized: &Path,
+    target_path: &Path,
+    hidden: bool,
+    ignore_git: bool,
+    root: &Path,
+    exclude_paths: &[PathBuf],
+    exclude: Option<&Path>,
+) -> io::Result<()>
+{
+    if entry_as_path_canonicalized.is_dir()
+    {
+        if !is_hidden(entry_as_path_canonicalized, hidden, ignore_git, root)
+        {
+            let entry_stripped_by_target_path = entry_as_path_canonicalized
+                .strip_prefix(target_path)
+                .unwrap_or(entry_as_path_canonicalized);
+            let genesis_dir = &root.join(entry_stripped_by_target_path);
+            fs::create_dir_all(genesis_dir)?;
+            filter_paths(
+                entry_as_path_canonicalized,
+                genesis_dir,
+                hidden,
+                ignore_git,
+                exclude_paths,
+            )?;
+        }
+    }
+    else if entry_as_path_canonicalized.is_file()
+        && !is_hidden(entry_as_path_canonicalized, hidden, ignore_git, root)
+    {
+        let entry_stripped_by_target_path = entry_as_path_canonicalized
+            .strip_prefix(target_path)
+            .unwrap_or(entry_as_path_canonicalized);
+        let genesis_path = &root.join(entry_stripped_by_target_path);
+        let genesis_path_parent = genesis_path.parent().unwrap_or(root);
+        if let Some(excluded_path) = exclude
+        {
+            if !excluded_path.as_os_str().is_empty()
+            {
+                let is_stripped_genesis_path_parent =
+                    genesis_path_parent.strip_prefix(excluded_path);
+                if is_stripped_genesis_path_parent.is_ok() && (*genesis_path_parent != *root)
+                {
+                    warn!(
+                        "⚠️ Adding file `{}` that is WITHIN an EXCLUDED directory `{}`.",
+                        entry_as_path_canonicalized.display(),
+                        &excluded_path.display()
+                    );
+                }
+            }
+        }
+        fs::create_dir_all(genesis_path_parent)?;
+        fs::copy(entry_as_path_canonicalized, genesis_path)?;
+    }
+    Ok(())
+}
+
 fn filter_paths(
     target_path: &Path,
     root: &Path,
@@ -69,44 +127,39 @@ fn filter_paths(
         let entry_as_path = &entry.path();
         let entry_as_path_canonicalized =
             &entry_as_path.canonicalize().unwrap_or(entry_as_path.to_path_buf());
-        if !is_hidden(&entry, hidden, ignore_git, root)
+        if exclude_paths.is_empty()
+        {
+            helper_archiver(
+                entry_as_path_canonicalized,
+                target_path,
+                hidden,
+                ignore_git,
+                root,
+                exclude_paths,
+                None,
+            )?;
+        }
+        else
         {
             for exclude in exclude_paths
             {
                 let is_stripped = entry_as_path_canonicalized.strip_prefix(exclude);
-                if is_stripped.is_err() && entry_as_path_canonicalized.is_dir()
+                debug!(?is_stripped, ?entry_as_path_canonicalized, ?exclude);
+                if is_stripped.is_err()
                 {
-                    let entry_stripped_by_target_path = entry_as_path_canonicalized
-                        .strip_prefix(target_path)
-                        .unwrap_or(target_path);
-                    let genesis_dir = &root.join(entry_stripped_by_target_path);
-                    fs::create_dir_all(genesis_dir)?;
-                    filter_paths(
+                    helper_archiver(
                         entry_as_path_canonicalized,
-                        genesis_dir,
+                        target_path,
                         hidden,
                         ignore_git,
+                        root,
                         exclude_paths,
+                        Some(exclude),
                     )?;
                 }
-                if entry_as_path_canonicalized.is_file() && is_stripped.is_err()
+                else if is_stripped.is_ok()
                 {
-                    let entry_stripped_by_target_path = entry_as_path_canonicalized
-                        .strip_prefix(target_path)
-                        .unwrap_or(target_path);
-                    let genesis_path = &root.join(entry_stripped_by_target_path);
-                    let genesis_path_parent = genesis_path.parent().unwrap_or(root);
-                    let is_stripped_genesis_path_parent = genesis_path_parent.strip_prefix(exclude);
-                    if is_stripped_genesis_path_parent.is_ok() && (*genesis_path_parent != *root)
-                    {
-                        warn!(
-                            "⚠️ Adding file `{}` that is WITHIN an EXCLUDED directory `{}`.",
-                            entry_as_path_canonicalized.display(),
-                            &exclude.display()
-                        );
-                    }
-                    fs::create_dir_all(genesis_path_parent)?;
-                    fs::copy(entry_as_path_canonicalized, genesis_path)?;
+                    debug!("Path was not included: {}", &entry_as_path_canonicalized.display());
                 }
             }
         }
@@ -174,6 +227,7 @@ pub fn roast_opts(roast_args: cli::RoastArgs, start_trace: bool) -> io::Result<(
     exclude_canonicalized_path = exclude_canonicalized_path
         .iter()
         .map(|p| target_path.join(p).canonicalize().unwrap_or_default())
+        .filter(|p| !p.as_os_str().is_empty())
         .collect();
 
     debug!(?exclude_canonicalized_path);

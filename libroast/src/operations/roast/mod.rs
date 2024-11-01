784931ca124c84222198264ba0b91911e0213a66
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2024-11-01 17:02:13 +0800

    use rayon while you can
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

--- libroast/src/operations/roast/mod.rs
+++ libroast/src/operations/roast/mod.rs
@@ -284,6 +284,7 @@ pub fn roast_opts(roast_args: cli::RoastArgs, start_trace: bool) -> io::Result<(
 
     let archive_files: Vec<PathBuf> = WalkDir::new(workdir)
         .into_iter()
+        .par_bridge()
         .flatten()
         .map(|f| {
             debug!(?f);

2bab074ef4e70eb5588b69a741772734a607947d
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2025-05-17 13:51:59 +0800

    feature(roast-scm): finalise feature and provide improvements
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

--- libroast/src/operations/roast_scm/mod.rs
+++ libroast/src/operations/roast_scm/mod.rs
@@ -25,6 +25,7 @@ use std::{
         Path,
         PathBuf,
     },
+    str::FromStr,
 };
 use tracing::{
     Level,
@@ -33,6 +34,10 @@ use tracing::{
     info,
     trace,
     warn,
+};
+use url::{
+    ParseError,
+    Url,
 };
 
 fn git_clone2(url: &str, local_clone_dir: &Path, revision: &str, depth: i32) -> io::Result<()>
@@ -68,6 +73,7 @@ fn git_clone2(url: &str, local_clone_dir: &Path, revision: &str, depth: i32) -> 
         error!(?err);
         io::Error::new(io::ErrorKind::Other, err.to_string())
     })?;
+
     match reference
     {
         Some(gitref) => local_repository.set_head(gitref.name().ok_or_else(|| {
@@ -100,6 +106,36 @@ fn git_clone2(url: &str, local_clone_dir: &Path, revision: &str, depth: i32) -> 
     Ok(())
 }
 
+fn process_filename_prefix_from_url(url_string: &str, revision: &str) -> io::Result<String>
+{
+    let url = Url::parse(url_string).map_err(|err| {
+        error!(?err);
+        io::Error::new(io::ErrorKind::InvalidInput, "Not able to parse URL string!")
+    })?;
+    let path_segments = url
+        .path_segments()
+        .map(|c| c.collect::<Vec<&str>>())
+        .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "Cannot generate basename!"))?;
+    debug!(?path_segments);
+    // NOTE: Some people copy `.git` e.g. https://github.com/octocat/octocat.git.
+    let basename = match path_segments[1].rsplit_once(".git")
+    {
+        Some((basename, _)) => basename,
+        None => path_segments[1],
+    };
+
+    let filename = if revision.is_empty()
+    {
+        basename.to_string()
+    }
+    else
+    {
+        format!("{}-{}", basename, revision)
+    };
+
+    Ok(filename)
+}
+
 pub fn roast_scm_opts(roast_scm_args: &RoastScmArgs, start_trace: bool) -> io::Result<()>
 {
     if start_trace
@@ -108,21 +144,48 @@ pub fn roast_scm_opts(roast_scm_args: &RoastScmArgs, start_trace: bool) -> io::R
     }
     info!("⛓️🔥 Starting Roast SCM!");
     debug!(?roast_scm_args);
-    let local_clone_dir = tempfile::TempDir::new()?;
-    git_clone2(
+    let workdir = tempfile::TempDir::new()?;
+    let filename_prefix = process_filename_prefix_from_url(
         &roast_scm_args.git_repository_url,
-        local_clone_dir.path(),
         &roast_scm_args.revision,
-        roast_scm_args.depth,
     )?;
+    let local_clone_dir = workdir.path().join(&filename_prefix);
+
+    let outfile = match roast_scm_args.outfile.clone()
+    {
+        Some(outfile) => outfile,
+        None =>
+        {
+            // TODO: Maybe create a function that gives the file extension??
+            let extension = match &roast_scm_args.compression
+            {
+                crate::common::Compression::Gz => "tar.gz",
+                crate::common::Compression::Xz => "tar.xz",
+                crate::common::Compression::Zst | crate::common::Compression::Zstd => "tar.zst",
+                crate::common::Compression::Bz2 => "tar.bz",
+                crate::common::Compression::Not => "tar",
+            };
+            let full_filename = format!("{}.{}", filename_prefix, extension);
+            Path::new(&full_filename).to_path_buf()
+        }
+    };
+
+    let git_url = &roast_scm_args.git_repository_url.to_string();
+
+    info!(?git_url, "🫂 Cloning remote repository.");
+    info!(?local_clone_dir, "🏃 Cloning to local directory...");
+    git_clone2(&git_url, &local_clone_dir, &roast_scm_args.revision, roast_scm_args.depth)?;
+    info!(?git_url, "🫂 Finished cloning remote repository.");
+    info!("🍄 Cloned to `{}`.", local_clone_dir.display());
+
     let roast_args = RoastArgs {
-        target: local_clone_dir.path().to_path_buf(),
+        target: local_clone_dir,
         include: None,
         exclude: roast_scm_args.exclude.clone(),
         additional_paths: None,
-        outfile: roast_scm_args.outfile.clone(),
+        outfile,
         outdir: roast_scm_args.outdir.clone(),
-        preserve_root: false,
+        preserve_root: true,
         reproducible: roast_scm_args.reproducible,
         ignore_git: roast_scm_args.ignore_git,
         ignore_hidden: roast_scm_args.ignore_hidden,

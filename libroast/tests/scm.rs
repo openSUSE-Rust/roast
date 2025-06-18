d923e428f2b62697df0b2d267ded8a05c12b5286
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2025-06-18 18:21:49 +0800

    tests: add deno and jay, they're known to contain submodules
    jay was the cause of adding such a fix prior this commit.
    
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

--- libroast/tests/scm.rs
+++ libroast/tests/scm.rs
@@ -166,3 +166,167 @@ fn different_revisions_pointing_to_the_same_commit_produce_the_same_filenames() 
     assert_eq!(hasher1.finalize(), hasher2.finalize());
     Ok(())
 }
+
+
+#[test]
+fn repo_with_submodules_1() -> io::Result<()>
+{
+    let tmp_binding1 = tempfile::TempDir::new().map_err(|err| {
+        error!(?err, "Failed to create temporary directory");
+        err
+    })?;
+    let tmp_binding2 = tempfile::TempDir::new().map_err(|err| {
+        error!(?err, "Failed to create temporary directory");
+        err
+    })?;
+    let mut hasher1 = Keccak256::new();
+    let mut hasher2 = Keccak256::new();
+    let outdir1 = tmp_binding1.path();
+    let outdir2 = tmp_binding2.path();
+    let r1 = RoastScmArgs {
+        set_version: None,
+        set_name: None,
+        changesgenerate: false,
+        changesauthor: None,
+        changesemail: None,
+        changesoutfile: None,
+        git_repository_url: "https://github.com/denoland/deno".to_string(),
+        exclude: None,
+        revision: "main".to_string(),
+        versionrewriteregex: None,
+        versionrewritepattern: None,
+        depth: 0,
+        is_temporary: true,
+        outfile: None,
+        outdir: Some(outdir1.to_path_buf()),
+        reproducible: true,
+        ignore_git: true,
+        ignore_hidden: false,
+        compression: libroast::common::Compression::default(),
+    };
+    libroast::operations::roast_scm::roast_scm_opts(None, &r1, false)?;
+    let r2 = RoastScmArgs {
+        set_version: None,
+        set_name: None,
+        changesgenerate: false,
+        changesauthor: None,
+        changesemail: None,
+        changesoutfile: None,
+        git_repository_url: "https://github.com/denoland/deno".to_string(),
+        exclude: None,
+        revision: "main".to_string(),
+        versionrewriteregex: None,
+        versionrewritepattern: None,
+        depth: 0,
+        is_temporary: true,
+        outfile: None,
+        outdir: Some(outdir2.to_path_buf()),
+        reproducible: true,
+        ignore_git: true,
+        ignore_hidden: false,
+        compression: libroast::common::Compression::default(),
+    };
+    libroast::operations::roast_scm::roast_scm_opts(None, &r2, false)?;
+    let read_dir1 = read_dir(outdir1)?;
+    let read_dir2 = read_dir(outdir2)?;
+    let Some(file1) = read_dir1.flatten().find(|entry| entry.path().is_file())
+    else
+    {
+        return Err(io::Error::new(io::ErrorKind::NotFound, "file1 not found."));
+    };
+    let Some(file2) = read_dir2.flatten().find(|entry| entry.path().is_file())
+    else
+    {
+        return Err(io::Error::new(io::ErrorKind::NotFound, "file2 not found."));
+    };
+    assert_eq!(file1.file_name(), file2.file_name());
+    let hash1 = read(file1.path())?;
+    let hash2 = read(file2.path())?;
+    hasher1.update(hash1);
+    hasher2.update(hash2);
+    assert_eq!(hasher1.finalize(), hasher2.finalize());
+    Ok(())
+}
+
+
+#[test]
+fn repo_with_submodules_2() -> io::Result<()>
+{
+    let tmp_binding1 = tempfile::TempDir::new().map_err(|err| {
+        error!(?err, "Failed to create temporary directory");
+        err
+    })?;
+    let tmp_binding2 = tempfile::TempDir::new().map_err(|err| {
+        error!(?err, "Failed to create temporary directory");
+        err
+    })?;
+    let mut hasher1 = Keccak256::new();
+    let mut hasher2 = Keccak256::new();
+    let outdir1 = tmp_binding1.path();
+    let outdir2 = tmp_binding2.path();
+    let r1 = RoastScmArgs {
+        set_version: None,
+        set_name: None,
+        changesgenerate: false,
+        changesauthor: None,
+        changesemail: None,
+        changesoutfile: None,
+        git_repository_url: "https://github.com/mahkoh/jay".to_string(),
+        exclude: None,
+        revision: "master".to_string(),
+        versionrewriteregex: None,
+        versionrewritepattern: None,
+        depth: 0,
+        is_temporary: true,
+        outfile: None,
+        outdir: Some(outdir1.to_path_buf()),
+        reproducible: true,
+        ignore_git: true,
+        ignore_hidden: false,
+        compression: libroast::common::Compression::default(),
+    };
+    libroast::operations::roast_scm::roast_scm_opts(None, &r1, false)?;
+    let r2 = RoastScmArgs {
+        set_version: None,
+        set_name: None,
+        changesgenerate: false,
+        changesauthor: None,
+        changesemail: None,
+        changesoutfile: None,
+        git_repository_url: "https://github.com/mahkoh/jay".to_string(),
+        exclude: None,
+        revision: "master".to_string(),
+        versionrewriteregex: None,
+        versionrewritepattern: None,
+        depth: 0,
+        is_temporary: true,
+        outfile: None,
+        outdir: Some(outdir2.to_path_buf()),
+        reproducible: true,
+        ignore_git: true,
+        ignore_hidden: false,
+        compression: libroast::common::Compression::default(),
+    };
+    libroast::operations::roast_scm::roast_scm_opts(None, &r2, false)?;
+    let read_dir1 = read_dir(outdir1)?;
+    let read_dir2 = read_dir(outdir2)?;
+    let Some(file1) = read_dir1.flatten().find(|entry| entry.path().is_file())
+    else
+    {
+        return Err(io::Error::new(io::ErrorKind::NotFound, "file1 not found."));
+    };
+    let Some(file2) = read_dir2.flatten().find(|entry| entry.path().is_file())
+    else
+    {
+        return Err(io::Error::new(io::ErrorKind::NotFound, "file2 not found."));
+    };
+    assert_eq!(file1.file_name(), file2.file_name());
+    let hash1 = read(file1.path())?;
+    let hash2 = read(file2.path())?;
+    hasher1.update(hash1);
+    hasher2.update(hash2);
+    assert_eq!(hasher1.finalize(), hasher2.finalize());
+    Ok(())
+}
+
+

b5513373cc95e5dc6bc5a9f695cd3f9734988531
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2025-06-22 11:16:10 +0800

    fix(tests): add missing fields
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

--- libroast/tests/obs_scm.rs
+++ libroast/tests/obs_scm.rs
@@ -48,15 +48,16 @@ fn different_revisions_pointing_to_the_same_commit_produce_the_same_files() -> i
     std::env::set_current_dir(&outdir)?;
     fs::copy(&specfile_path, &outdir.join("obs-service-cargo.spec"))?;
     let r1 = RoastScmArgs {
+        subcommands: None,
         changesgenerate: true,
         changesauthor: Some("Soc Virnyl Estela".to_string()),
         changesemail: Some("uncomfyhalomacro@opensuse.org".to_string()),
         changesoutfile: None,
         set_version: None,
         set_name: None,
-        git_repository_url: "https://github.com/openSUSE-Rust/obs-service-cargo".to_string(),
+        git_repository_url: Some("https://github.com/openSUSE-Rust/obs-service-cargo".to_string()),
         exclude: None,
-        revision: "v5.1.0".to_string(),
+        revision: Some("v5.1.0".to_string()),
         versionrewriteregex: None,
         versionrewritepattern: None,
         depth: 0,
@@ -76,15 +77,16 @@ fn different_revisions_pointing_to_the_same_commit_produce_the_same_files() -> i
     let buf1 = read(outdir.join("r1.tar.zst"))?;
     hasher1.update(buf1);
     let r2 = RoastScmArgs {
+        subcommands: None,
         changesgenerate: false,
         changesauthor: None,
         changesemail: None,
         changesoutfile: None,
         set_version: None,
         set_name: None,
-        git_repository_url: "https://github.com/openSUSE-Rust/obs-service-cargo".to_string(),
+        git_repository_url: Some("https://github.com/openSUSE-Rust/obs-service-cargo".to_string()),
         exclude: None,
-        revision: "2910335f66158a658dfeebca8b8bf6cfc09ba1c0".to_string(),
+        revision: Some("2910335f66158a658dfeebca8b8bf6cfc09ba1c0".to_string()),
         versionrewriteregex: None,
         versionrewritepattern: None,
         depth: 0,
@@ -131,15 +133,16 @@ fn different_revisions_pointing_to_the_same_commit_produce_the_same_filenames() 
     fs::copy(&specfile_path, &outdir2.join("obs-service-cargo.spec"))?;
     std::env::set_current_dir(&outdir1)?;
     let r1 = RoastScmArgs {
+        subcommands: None,
         changesgenerate: false,
         changesauthor: None,
         changesemail: None,
         changesoutfile: None,
         set_version: None,
         set_name: None,
-        git_repository_url: "https://github.com/openSUSE-Rust/obs-service-cargo".to_string(),
+        git_repository_url: Some("https://github.com/openSUSE-Rust/obs-service-cargo".to_string()),
         exclude: None,
-        revision: "v5.1.0".to_string(),
+        revision: Some("v5.1.0".to_string()),
         versionrewriteregex: None,
         versionrewritepattern: None,
         depth: 0,
@@ -154,15 +157,16 @@ fn different_revisions_pointing_to_the_same_commit_produce_the_same_filenames() 
     libroast::operations::roast_scm::roast_scm_opts(None, &r1, false)?;
     std::env::set_current_dir(&outdir2)?;
     let r2 = RoastScmArgs {
+        subcommands: None,
         changesgenerate: false,
         changesauthor: None,
         changesemail: None,
         changesoutfile: None,
         set_version: None,
         set_name: None,
-        git_repository_url: "https://github.com/openSUSE-Rust/obs-service-cargo".to_string(),
+        git_repository_url: Some("https://github.com/openSUSE-Rust/obs-service-cargo".to_string()),
         exclude: None,
-        revision: "2910335f66158a658dfeebca8b8bf6cfc09ba1c0".to_string(),
+        revision: Some("2910335f66158a658dfeebca8b8bf6cfc09ba1c0".to_string()),
         versionrewriteregex: None,
         versionrewritepattern: None,
         depth: 0,

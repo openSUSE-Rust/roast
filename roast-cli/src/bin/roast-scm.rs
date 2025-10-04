ce5048e01a07e5b68d644b00686060a291fd5a1f
Soc Virnyl Estela <contact@uncomfyhalomacro.pl>
2025-10-05 00:57:48 +0800

    style: run cargo +nightly format
    Signed-off-by: Soc Virnyl Estela <contact@uncomfyhalomacro.pl>

--- roast-cli/src/bin/roast-scm.rs
+++ roast-cli/src/bin/roast-scm.rs
@@ -1,7 +1,9 @@
 use roast_cli::roast_scm::roast_scm_cli_stub;
 
-fn main() {
-    if roast_scm_cli_stub().is_err() {
+fn main()
+{
+    if roast_scm_cli_stub().is_err()
+    {
         std::process::exit(1)
     }
 }
